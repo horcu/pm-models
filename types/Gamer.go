a2f5cb4bfa76020558fcf0795f826902b1204ae4
Horatio Cummings <horatio.cummings@gmail.com>
2024-11-13 21:54:19 -0500

    sync

--- types/Gamer.go
+++ types/Gamer.go
@@ -1,15 +1,15 @@
 package types
 
 type Gamer struct {
-	Bin         string     `json:"bin"`
-	GameId      string     `json:"game_id"`
-	CharacterId string     `json:"character_id"`
-	Name        string     `json:"name"`
-	Fate        *Fate      `json:"fate,omitempty"`
-	ImageUrl    string     `json:"image_url"`
-	IsAlive     bool       `json:"is_alive"`
-	SeenSteps   []string   `json:"seen_steps"`  //list of step bins
-	VotedSteps  []string   `json:"voted_steps"` // map of cycles and list of step bins
-	Abilities   []*Ability `json:"abilities,omitempty"`
-	Metrics     *Metrics   `json:"metrics,omitempty"`
+	Bin         string              `json:"bin"`
+	GameId      string              `json:"game_id"`
+	CharacterId string              `json:"character_id"`
+	Name        string              `json:"name"`
+	Fate        *Fate               `json:"fate,omitempty"`
+	ImageUrl    string              `json:"image_url"`
+	IsAlive     bool                `json:"is_alive"`
+	SeenSteps   []string            `json:"seen_steps"`  //list of step bins
+	VotedSteps  []string            `json:"voted_steps"` // map of cycles and list of step bins
+	Abilities   map[string]*Ability `json:"abilities,omitempty"`
+	Metrics     *Metrics            `json:"metrics,omitempty"`
 }
