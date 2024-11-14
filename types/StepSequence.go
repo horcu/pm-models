e71f55cbded9a50752c0cb008fad31d7547f1f5d
Horatio Cummings <horatio.cummings@gmail.com>
2024-11-14 02:34:25 -0500

    sync

--- types/StepSequence.go
+++ types/StepSequence.go
@@ -2,6 +2,6 @@ package types
 
 type StepSequence struct {
 	SDuration string `json:"s_duration"`
-	CStep     string `json:"c_step"`
+	CStep     Step   `json:"c_step"`
 	GameId    string `json:"game_id"`
 }
