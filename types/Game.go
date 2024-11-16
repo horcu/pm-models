9e5d49f1100ce642f8a947e0500623bba4c43d42
Horatio Cummings <horatio.cummings@gmail.com>
2024-11-16 13:48:59 -0500

    added main suspects to Game struct. added Sums prop to Metrics struct

--- types/Game.go
+++ types/Game.go
@@ -19,6 +19,7 @@ type Game struct {
 	IsIntroductions   bool                      `json:"is_introductions,omitempty"`
 	IsResultStep      bool                      `json:"is_result_step,omitempty"`
 	Creator           *Player                   `json:"creator,omitempty"`
+	MainSuspects      map[string][]*Gamer       `json:"main_suspects,omitempty"`
 	NightCycles       int                       `json:"cycles,omitempty"`
 	AllVotesSubmitted bool                      `json:"all_votes_submitted,omitempty"`
 	Steps             map[string]*Step          `json:"steps,omitempty"`
