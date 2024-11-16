ef2f52876048114fb56a83e6797080db3a26a8ee
Horatio Cummings <horatio.cummings@gmail.com>
2024-11-16 13:58:55 -0500

    added main suspects to Game struct. added Sums and Single prop to Metrics struct

--- types/Metrics.go
+++ types/Metrics.go
@@ -1,6 +1,7 @@
 package types
 
 type Metrics struct {
-	Values map[string][]string `json:"values,omitempty"` // enum metricsValues are keys
-	Sums   map[string]int      `json:"sums,omitempty"`   // enum metricsValues are keys
+	Values map[string][]string `json:"values,omitempty"`  // enum metricsValues are keys
+	Single map[string]string   `json:"singles,omitempty"` // enum metricsValues are keys
+	Sums   map[string]int      `json:"sums,omitempty"`    // enum metricsValues are keys
 }
