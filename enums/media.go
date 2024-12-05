58ce35be39d6e2064e90f039bd03afcd26cd02a8
Horatio Cummings <horatio.cummings@gmail.com>
2024-12-04 23:06:56 -0500

    Added StepBin to Messages

--- enums/media.go
+++ enums/media.go
@@ -6,9 +6,10 @@ type MediaType string
 
 // Declare related constants for each ability starting with index 1
 const (
-	Audio MediaType = "audio"
-	Video MediaType = "video"
-	Text  MediaType = "text"
+	Audio     MediaType = "audio"
+	Video     MediaType = "video"
+	Text      MediaType = "text"
+	GamerVote MediaType = "vote"
 )
 
 func (s MediaType) String() string {
