2bd85d45c253afd18944360890080e4c85a3cbae
Horatio Cummings <horatio.cummings@gmail.com>
2024-12-09 14:40:23 -0500

    Added TimeSync and GameEvents

---
+++ types/Events.go
@@ -0,0 +1,15 @@
+package types
+
+type Events struct {
+	IsVoting          bool `json:"is_voting,omitempty"`
+	IsGamePlay        bool `json:"is_game_play,omitempty"`
+	IsInTransition    bool `json:"is_in_transition,omitempty"`
+	IsInDiscussion    bool `json:"is_in_discussion,omitempty"`
+	IsInDefend        bool `json:"is_in_defend,omitempty"`
+	IsIntroductions   bool `json:"is_introductions,omitempty"`
+	IsResultStep      bool `json:"is_result_step,omitempty"`
+	AllVotesSubmitted bool `json:"all_votes_submitted,omitempty"`
+	IsDaytime         bool `json:"is_daytime,omitempty"`
+	ExplanationSeen   bool `json:"explanation_seen,omitempty"`
+	FirstDayCompleted bool `json:"first_day_completed,omitempty"`
+}
---
+++ types/TimeSync.go
@@ -0,0 +1,6 @@
+package types
+
+type TimeSync struct {
+	StartTime string `json:"start_time,omitempty"`
+	EndTime   string `json:"end_time,omitempty"`
+}
