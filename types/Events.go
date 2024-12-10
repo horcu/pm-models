b32dfea75a03c253045f514005a4343b834851df
Horatio Cummings <horatio.cummings@gmail.com>
2024-12-10 09:42:19 -0500

    Added IsCharacterStep and IsGameover to the events struct

--- types/Events.go
+++ types/Events.go
@@ -6,6 +6,8 @@ type Events struct {
 	IsInTransition    bool `json:"is_in_transition,omitempty"`
 	IsInDiscussion    bool `json:"is_in_discussion,omitempty"`
 	IsInDefend        bool `json:"is_in_defend,omitempty"`
+	IsCharacterStep   bool `json:"is_character_step,omitempty"`
+	IsGameOver        bool `json:"is_game_over,omitempty"`
 	IsIntroductions   bool `json:"is_introductions,omitempty"`
 	IsResultStep      bool `json:"is_result_step,omitempty"`
 	AllVotesSubmitted bool `json:"all_votes_submitted,omitempty"`
