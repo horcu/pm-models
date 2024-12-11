10699f0daed2b8dddad3179df6ee41078b284b37
Horatio Cummings <horatio.cummings@gmail.com>
2024-12-11 03:31:04 -0500

    Added PlayerIds to ServerInfo

--- types/ServerInfo.go
+++ types/ServerInfo.go
@@ -1,10 +1,11 @@
 package types
 
 type ServerInfo struct {
-	Name      string `json:"name,omitempty"`
-	Address   string `json:"address,omitempty"`
-	Port      int    `json:"port,omitempty"`
-	CreatorId string `json:"creator_id,omitempty"`
-	GroupId   string `json:"group_id,omitempty"`
-	Level     string `json:"level,omitempty"`
+	Name      string   `json:"name,omitempty"`
+	Address   string   `json:"address,omitempty"`
+	Port      int      `json:"port,omitempty"`
+	CreatorId string   `json:"creator_id,omitempty"`
+	PlayerIds []string `json:"player_ids,omitempty"`
+	GroupId   string   `json:"group_id,omitempty"`
+	Level     string   `json:"level,omitempty"`
 }
